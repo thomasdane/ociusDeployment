b475c420fc5b1f6940f22be95ee95115b7ebbf92
Tom Dane <tjdane@gmail.com>
2018-02-12 17:53:24 +0000

    Deploy ASG

--- main.tf
+++ main.tf
@@ -36,6 +36,9 @@ resource "aws_autoscaling_group" "example" {
     min_size = 1
     max_size = 1
 
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+
     tag {
         key = "Name"
         value = "terraform-asg-example"
@@ -45,6 +48,7 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_elb" "example" {
     name = "terraform-asg-example"
+    security_groups = ["${aws_security_group.elb.id}"]
     availability_zones = ["${data.aws_availability_zones.all.names}"]
 
     listener {
@@ -53,10 +57,34 @@ resource "aws_elb" "example" {
         instance_port = "${var.server_port}"
         instance_protocol = "http"
     }
+
+    health_check {
+        healhy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}/"
+    }
 }
 
 resource "aws_security_group" "elb" {
     name = "terraform-example-elb"
 
-    ingress
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [0.0.0.0/0]
+    }
+}
+
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
